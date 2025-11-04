83c9d22cbc89842e81b33ebf0a879b63531ded6d
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2025-11-04 03:30:51 -0700

    add header and navbar components and test layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "./Navbar";
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>folio</h1>
+      <Navbar></Navbar>
+    </header>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
